8951f24a5166f6afd8351d8fd7041524170c9ba1
Mona Sharma <mona.deligence@gmail.com>
2025-10-13 23:01:23 +0530

    Initial Gatsby project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,36 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Deligence`,
+    description: `Deligence site powered by WordPress and Gatsby`,
+    author: `Deligence Team`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `http://localhost/Testingsite/graphql`,
+        schema: {
+          timeout: 60000, // 60s
+          perPage: 20,    // fetch data in smaller chunks
+        },
+        type: {
+          MediaItem: {
+            localFile: {
+              requestConcurrency: 3,      // download 3 files at once
+              maxFileSizeBytes: 20000000, // limit to 20MB
+            },
+          },
+        },
+        develop: {
+          hardCacheMediaFiles: true,
+        },
+        production: {
+          hardCacheMediaFiles: true,
+        },
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+};
