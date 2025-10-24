a9488971d3e32872b0b5d425ef611da26a951736
Monasharma5 <146428468+Monasharma5@users.noreply.github.com>
2025-10-24 15:50:52 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,59 @@
+
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query FooterQuery {
+      wp {
+        generalSettings {
+          title
+        }
+        menus(where: { location: FOOTER }) {
+          nodes {
+            menuItems {
+              nodes {
+                id
+                label
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const menuItems = data.wp.menus.nodes[0]?.menuItems.nodes || []
+
+  return (
+    <footer
+      style={{
+        background: "#eee",
+        padding: "1rem",
+        textAlign: "center",
+        marginTop: "2rem",
+      }}
+    >
+      <p>© {new Date().getFullYear()} {data.wp.generalSettings.title}</p>
+      <nav style={{ marginTop: "1rem" }}>
+        {menuItems.map(item => (
+          <Link
+            key={item.id}
+            to={item.url.replace("http://localhost/Testingsite", "")}
+            style={{ margin: "0 0.5rem" }}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
+
+/*
 import React from "react"
 
 const Footer = () => (
@@ -7,3 +63,4 @@ const Footer = () => (
 )
 
 export default Footer
+*/
